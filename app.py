c83899709b0965cfcba4bf9ad1121abc4309b72b
Michael Arciola <mra33@live.com>
2016-11-28 11:31:13 -0500

    added layout.html

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! - Michael'
+    #return 'Hello World! - Michael'
+	return render_template('layout.html')
 
 if __name__ == '__main__':
     app.run()
