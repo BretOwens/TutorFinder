b139165349b3741e179575f3a9cbbed40fc9f8b8
Michael Arciola <mra33@live.com>
2016-11-29 23:59:22 -0500

    Reverting back to SQLAlchemy

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-import pypyodbc
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-myConnection = pypyodbc.connect(os.environ['DATABASE_URL'])
-myCursor = myConnection.cursor()
-myCursor.executre('SELECT * FROM Tutors')
-rows = myCursor.fetchall();
-
-for r in rows:
-	print(f)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://m462isa2dh6cvxue:jfl50lzw43d657yq@nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rumyr9ysvijlvzqd?sslca=rds-combined-ca-bundle.pem&ssl-verify-server-cert'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
