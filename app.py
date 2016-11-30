2101520492a45d686ff73038ae28f844bb864c6a
Michael Arciola <mra33@live.com>
2016-11-29 23:18:09 -0500

    Added pypyodbc cursor

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://m462isa2dh6cvxue:jfl50lzw43d657yq@nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rumyr9ysvijlvzqd?sslca=rds-combined-ca-bundle.pem&ssl-verify-server-cert'
 #db = SQLAlchemy(app)
 
-myConnection = pypyodbc.connect(os.environ['DATABASE_URL']
+myConnection = pypyodbc.connect(os.environ['DATABASE_URL'])
+myCursor = myConnection.cursor()
+myCursor.executre('SELECT * FROM Tutors')
+rows = myCursor.fetchall();
+
+for r in rows:
+	print(f)
 
 @app.route('/')
 def hello_world():
