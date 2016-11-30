998cd8e763fffc96527e408fd7197340c19ba9e8
Michael Arciola <mra33@live.com>
2016-11-29 23:21:29 -0500

    Added pypyodbc V1.3.3 to requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-#from flask_sqlalchemy import SQLAlchemy
 import pypyodbc
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://m462isa2dh6cvxue:jfl50lzw43d657yq@nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rumyr9ysvijlvzqd?sslca=rds-combined-ca-bundle.pem&ssl-verify-server-cert'
-#db = SQLAlchemy(app)
 
 myConnection = pypyodbc.connect(os.environ['DATABASE_URL'])
 myCursor = myConnection.cursor()
